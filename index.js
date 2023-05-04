f910cdacf26d03835297de856de2b51972e893dd
Karl <toom.karl42@gmail.com>
2023-05-04 21:15:19 +0300

    found out the api i wanted to use wont connect, restarting the project another time

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 //GAMES API https://www.freetogame.com/api/games
 
 //CERTAIN GAMES API https://www.freetogame.com/api/game?id=452
+
+async function getGames(){
+    const games = await fetch("https://api.openbrewerydb.org/v1/breweries")
+    const gamesData = await games.json()
+    console.log(gamesData)
+}
+getGames()
