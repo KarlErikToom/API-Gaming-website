0053d748dcddefea3cd1099182761686c2cf34de
Karl <toom.karl42@gmail.com>
2023-05-07 17:38:10 +0300

    made a quick landing page

--- index.js
+++ index.js
@@ -2,9 +2,28 @@
 
 //agents url https://bymykel.github.io/CSGO-API/api/en/agents.json
 
-async function getAgents(){
-  const agents = await fetch ("https://bymykel.github.io/CSGO-API/api/en/agents.json")
-  const agentsData = await agents.json()
-  console.log(agentsData)
+const agentListEl = document.querySelector(".agent-list");
+
+async function getAgents() {
+  const agents = await fetch(
+    "https://bymykel.github.io/CSGO-API/api/en/agents.json"
+  );
+  const agentsData = await agents.json();
+  agentListEl.innerHTML = agentsData.map((agent) => agentHTML(agent)).join("");
+ console.log(agentsData)
 }
-getAgents()
+getAgents();
+
+function agentHTML(agent) {
+  return;
+  `
+  <div class="agent-box">
+                <div class="agent-box__container">
+                    <img class="agent__img" src="${agent.image}" alt="">
+                    <h3>Agent's name ${agent.name} </h3>
+                    <p>agent's rarity ${agent.rarity}</p>
+                </div>
+            </div>
+  `;
+}
+agentHTML()
